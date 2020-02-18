27435d1bf6e1abf27f717c4c4e719143e6f573ba
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-02-18 21:14:35 +0800

    [refactor] Update prod server url

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const config = {
 	backendUrl:
 		process.env.NODE_ENV === 'development'
 			? 'http://localhost:9000/.netlify/functions/app'
-			: 'https://spotify-lambda.netlify.com/.netlify/functions/app',
+			: 'https://spotify-stats-server.netlify.com/.netlify/functions/app',
 };
 
 export default config;
